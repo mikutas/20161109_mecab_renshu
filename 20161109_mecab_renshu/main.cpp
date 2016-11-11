eeefa0135802f290cefba953f49e3cbbb7da27ce
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2016-11-11 10:31:27 +0900

    プロジェクト ファイルを追加します。

---
+++ 20161109_mecab_renshu/main.cpp
@@ -0,0 +1,116 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <mecab.h>
+
+using namespace std;
+
+vector<string> split(string s, string c) {
+	vector<string> ret;
+	for (int i = 0, n; i <= s.length(); i = n + 1) {
+		n = s.find_first_of(c, i);
+		if (n == string::npos)n = s.length();
+		string tmp = s.substr(i, n - i);
+		ret.push_back(tmp);
+	}
+	return ret;
+}
+
+int main() {
+	cout << "How many writers?" << endl;
+	int writers;
+	cin >> writers;
+	cout << "I got it." << endl;
+
+	// 作家ごとのTerm Frequency
+	vector<multimap<double, string>> tf_map_vec;
+	// Document Frequency
+	map<string, int> doc_freq;
+
+	for (int i = 0; i < writers; i++) {
+		int total_words = 0;
+		map<string, int> word_count;
+		multimap<double, string> term_freq;
+		map<string, int>::iterator it;
+
+		string input_file;
+		string str;
+		cout << "Drop file here and press ENTER." << endl;
+		cin >> input_file;
+		while (input_file != "x") {
+			//cout << input_file << endl;
+			ifstream ifs(input_file);
+			if (ifs.fail()) {
+				cout << "ファイル開けない" << endl;
+				exit(1);
+			}
+			char c;
+			while (ifs.get(c)) {
+				str.push_back(c);
+			}
+			cout << "next?" << endl;
+			cin >> input_file;
+		}
+		MeCab::Tagger *tagger = MeCab::createTagger("");
+		const MeCab::Node *node = tagger->parseToNode(str.c_str());
+
+		for (node = node->next; node->next; node = node->next) {
+			vector<string> strvec = split(node->feature, ",");
+			total_words++;
+			if (strvec[0] == "名詞" /*| strvec[0] == "動詞"*/) {
+				//cout << "counting..." << endl;
+				string noun = strvec[6];
+				it = doc_freq.find(noun);
+				if (it != doc_freq.end()) {
+					if (it->second < i) it->second++;
+				}
+				else {
+					doc_freq.insert(pair<string, int>(noun, 1));
+				}
+				it = word_count.find(noun);
+				if (it != word_count.end()) {
+					it->second += 1;
+				}
+				else {
+					word_count.insert(pair<string, int>(noun, 1));
+				}
+			}
+		}
+		delete tagger;
+
+		for (it = word_count.begin(); it != word_count.end(); it++) {
+			term_freq.insert(pair<double, string>((double)(it->second) / total_words, it->first));
+		}
+		tf_map_vec.push_back(term_freq);
+	}
+
+	vector<multimap<double, string>> tf_idf_vec;
+	for (int i = 0; i < writers; i++) {
+		multimap<double, string> tf_idf;
+		map<double, string>::iterator it;
+		for (it = tf_map_vec.at(i).begin(); it != tf_map_vec.at(i).end(); it++) {
+			double idf = log(writers) / (double)(doc_freq.find(it->second)->second);
+			double tf_idf_val = (it->first) * idf;
+			tf_idf.insert(pair<double, string>(tf_idf_val, it->second));
+		}
+		tf_idf_vec.push_back(tf_idf);
+	}
+
+	// 作家ごとにtf-idfトップいくつかを表示
+	cout << "How many words?" << endl;
+	int display_words;
+	cin >> display_words;
+	for (int i = 0; i < writers; i++) {
+		cout << i + 1 << "人目" << endl;
+		multimap<double, string>::reverse_iterator rit;
+		int j = 0;
+		for (rit = tf_idf_vec.at(i).rbegin(); j < min(tf_idf_vec.at(i).size(), display_words); rit++) {
+			cout << rit->second << endl;
+			j++;
+		}
+	}
+
+	return 0;
+}
